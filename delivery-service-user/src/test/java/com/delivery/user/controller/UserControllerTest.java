51230b5724f04940d836fee7ef9f57418e7450be
lewisseo91 <togokr@naver.com>
2023-05-21 10:11:27 +0900

    feat(user): controller test 케이스 추가

---
+++ delivery-service-user/src/test/java/com/delivery/user/controller/UserControllerTest.java
@@ -0,0 +1,23 @@
+package com.delivery.user.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("유저 컨트롤러 테스트")
+@SpringBootTest
+class UserControllerTest {
+
+    @DisplayName("회원 가입을 할 수 있다.")
+    @Test
+    public void signUpTest() {
+
+    }
+
+    @DisplayName("로그인을 할 수 있다.")
+    public void loginTest() {
+
+    }
+}
